b170602daba505ffc65fc39166bd11308805e483
Shujon Mollah <mollah@gmail.com>
2018-06-29 15:13:24 -0400

    Add top 20 names stat

--- stats.py
+++ stats.py
@@ -2,6 +2,10 @@ from config import PROVIDERS
 from database import dbopen
 from logzero import logger
 
+name_mapping = {
+    'uber_eats': 'payload_storePayload_stateMapDisplayInfo_available_title_text'
+}
+
 with dbopen() as cur:
     query = """
     SELECT count(*) from progress
@@ -15,3 +19,16 @@ with dbopen() as cur:
         """
         cur.execute(query)
         logger.info(f"{provider}: {cur.fetchone()}")
+
+    for provider in PROVIDERS:
+        name_column = name_mapping.get(provider, 'name')
+        query = f"""
+        SELECT {name_column}, count({name_column}) as count
+        FROM {provider}
+        GROUP BY {name_column}
+        ORDER BY count DESC;
+        """
+        cur.execute(query)
+        logger.info(f"{provider} top 20 restaurant name counts:")
+        for name, count in cur.fetchall()[:20]:
+            print(f"{name}={count}")
